5a4176156645126398b290cf21b37bc49d82d166
senthil <senthildeveloper4@gmail.com>
2025-12-23 01:48:54 +0530

    chore: Remove godotenv import and comment out .env file loading.

--- main.go
+++ main.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"backend/api"
-	"log"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 // Example usage
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file")
+	// }
 
 	r := gin.Default()
 
